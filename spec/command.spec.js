f8ab111d96bc8fbcdcdd31933c0d07d23dae9ac4
dreadstuff <gavinkolasa@gmail.com>
2024-08-01 19:57:58 -0500

    commit 1 for message and command js

--- spec/command.spec.js
+++ spec/command.spec.js
@@ -3,10 +3,22 @@ const Command = require('../command.js');
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
+
+//first three tests - commmand class, commandtype check, command value check - setting as moveCommand and testValue 
+
 describe("Command class", function() {
 
   it("throws error if command type is NOT passed into constructor as the first parameter", function() {
     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
   });
+
+  it("constructor sets command type", function() {
+    let moveCommand = new Command('MOVE', 12000);
+   expect(moveCommand.commandType).toBe('MOVE');
+})
+  it("constructor sets a value passed in as the 2nd argument", function() {
+    let testValue = new Command('MOVE', 12000);
+    expect(testValue.value).toBe(12000);
+})
 
 });
--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,5 +5,36 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
+    it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+        expect( function() { new Message();}).toThrow(new Error('Name is required'));
+        //  expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+    })
+    it("constructor sets name", function() {
+            let messageType = new Message('test name');
+           expect(messageType.name).toBe('test name');
+        })
+    it("contains a commands array passed into constructor as the 2nd argument", function() {
+            let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+            let testMess = new Message('test name', commands)
+            expect(testMess.commands).toStrictEqual(commands); //is this it? not sure what to expect here
+    })
+    })
+//    it("constructor sets name", function() {
 
-});
+//Reference command.spec.js
+// it("throws error if command type is NOT passed into constructor as the first parameter", function() {
+//     expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+//   });
+
+//   it("constructor sets command type", function() {
+//     let moveCommand = new Command('MOVE', 12000);
+//    expect(moveCommand.commandType).toBe('MOVE');
+// })
+//   it("constructor sets a value passed in as the 2nd argument", function() {
+//     let testValue = new Command('MOVE', 12000);
+//     expect(testValue.value).toBe(12000);
+
+//    })
+
+
+//});
