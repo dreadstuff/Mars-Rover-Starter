8f3dc5a468c440147e7ab1352548f9a747d984a7
dreadstuff <gavinkolasa@gmail.com>
2024-08-06 20:57:10 -0500

    push 1 rover

--- rover.js
+++ rover.js
@@ -1,5 +1,99 @@
 class Rover {
    // Write code here!
-}
+   constructor(position, mode = "NORMAL", generatorWatts = 110) {
+      //setting parameter to variables in constructor
+      this.position = position;
+      this.mode = mode;
+      this.generatorWatts = generatorWatts;
+      };
+
+      receiveMessage(message) {
+         const results = []; //init structure for array/results
+
+         //loop statement (for) to loop over each comm in comm array for msg obj
+            for (let i = 0; i > message.commands.length; i++) { //for loop setup
+               const command = message.commands[i];  //init command to message command loop, loop comm
+               let result = { completed: true };
+         //if else and else statements for diff comm types
+               if (command.commandType === "MODE_CHANGE") {
+                  this.mode = command.value; 
+               } else if (command.commandType === "MOVE") {
+                  if (this.mode === "LOW_POWER") {
+                     result.completed = false;
+                  } else {
+                     this.position = command.value;
+                  }
+               } else if (command.commandType === "STATUS_CHECK") {
+                  result.roverStatus = {
+                     mode: this.mode,
+                     generatorWatts: this.generatorWatts,
+                     position: this.position
+                  };
+                }
+                //push result array
+                results.push(result);
+            }
+         //return and object with message.name and results array 
+         return {message: message.name, results: results}
+       };
+  };
+
+//UNABLE TO FIX ISSUES BELOW, REBUILDING EASIER STRUCTURE DON'T USE~~~~~
+//       receiveMessage(message) {
+//       //declare output var (message) & init structure (results var array)
+//       let responder = {
+//       message: message.name,
+//       results: []
+//    }
+//    //for multiple commands - address each separately, not sure if a better method exists
+//    //breaks from while loop when none left
+//    while (message.commands.length > 0) {
+//       //if else statements for commands (move, low power, normal, status_check)
+//       if ( message.commands[0].commandType == 'MOVE')
+//          let moveObj = {};
+//          if (this.mode === 'LOW_POWER') {
+//             moveObj.completed = false;
+//          } else if (this.mode === 'NORMAL') {
+//             moveObj.completed = true;
+//             //update position to command value
+//             this.position = message.commands[0].value;
+//          }
+//          //push command object into results array, and splice the command
+//          responder.results.push(moveObj);
+//          message.commands.splice([0], 1)
+
+//          } else if ( message.commands[0].commandType == 'STATUS_CHECK') {
+//             let statObj = {
+//                completed: true,
+//             };
+//             statObj.roverStatus = {
+//                mode: this.mode,
+//                generatorWatts: this.generatorWatts,
+//                position: this.position
+//             }
+//             //push
+//             responder.results.push(statObj);
+//             message.commands.splice([0], 1)
+//          } else if (message.commands[0].commandType == 'MODE_CHANGE') {
+//             //mode updated to command value mode_change
+//             this.mode = message.commands[0].value
+//             let modeObj = {
+//                completed: true
+//             };
+//             responder.results.push(modeObj);
+//             message.commands.splice([0], 1)
+//          }
+//          return responder;
+//    }
+// }
+
+//command class reference
+// constructor(commandType, value) {
+//    this.commandType = commandType;
+//    if (!commandType) {
+//      throw Error("Command type required.");
+//    }
+//    this.value = value;
+//  }
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -7,7 +7,54 @@ const Command = require('../command.js');
 
 
 describe("Rover class", function() {
+//test 7
+  it("constructor sets position and default values for mode and generatorWatts", function() {
+    let roverObject = new Rover("position", "NORMAL", 110)
+      expect(roverObject.position).toEqual("position");
+      expect(roverObject.mode).toEqual("NORMAL");
+      expect(roverObject.generatorWatts).toEqual(110);
+//     let mode = "NORMAL";
+// //      expect(Rover.mode).ToBe("NORMAL"); //can I use expect here to check, or is let init fine?
+//     let generatorWatts = 110;
+//      expect(Rover.generatorWatts).ToBe(110);
+})
+//test 8
+  it("response returned by receiveMessage contains the name of the message", function() {
+    let command = new Command("MODE_CHANGE", "NORMAL"); //init command from command.js
+    let message = new Message("TA Power", command); //init message from message.js
+    let rover = new Rover(); //init rover class 
+    let response = rover.receiveMessage(message); //init response to received message within rover class
 
+    expect(response.message).toEqual("TA Power"); //expect exact response from rover message "TA Power"
+  });
+  
+  //test 9
+   it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
+
+   })
+
+  // it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
+  //   let rover = Rover();
+  //   let response = rover.receiveMessage(message);
+  //   expect(response.results).toHaveLength(2);
+  // })
   // 7 tests here!
 
 });
+
+//reference for previous tests from message
+// describe("Message class", function() {
+//   it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+//       expect( function() { new Message();}).toThrow(new Error('Name is required'));
+//       //  expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+//   })
+//   it("constructor sets name", function() {
+//           let messageType = new Message('test name');
+//          expect(messageType.name).toBe('test name');
+//       })
+//   it("contains a commands array passed into constructor as the 2nd argument", function() {
+//           let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+//           let testMess = new Message('test name', commands)
+//           expect(testMess.commands).toStrictEqual(commands); //is this it? not sure what to expect here
+//   })
+//   })
